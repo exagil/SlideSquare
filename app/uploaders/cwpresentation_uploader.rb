c3be4bacf60460df87e52d835fb89d0718d462f9
Chirag Aggarwal <chi6rag@gmail.com>
2015-01-16 15:14:28 +0530

    Configure AWS Details for production

--- app/uploaders/cwpresentation_uploader.rb
+++ app/uploaders/cwpresentation_uploader.rb
@@ -7,9 +7,12 @@ class CwpresentationUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
-
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
+  
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
@@ -38,9 +41,9 @@ class CwpresentationUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(ppt odp pptx pdf)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
