dcb415f355cf2a6d0d471e5b243e6295b741af5d
Chirag Aggarwal <chi6rag@gmail.com>
2015-01-18 16:30:29 +0530

    Add seeds data for categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,12 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# SLIDE
-presentation_a = Presentation.new(title: "The Rocket Games company culture", body: "The Rocket Games company culture - an open look at what defines our company. Who we are, and what we'd like to be.")
-presentation_a.save
 
-presentation_b = Presentation.new(title: "Behavioural Economics in Advertising Planning", body: "Presentation on Behavioural Economics and the use for it in brand/advertising planning within the creative industries.")
-presentation_b.save
+# Categories
 
-presentation_c = Presentation.new(title: "How to Make Presentations on The Go Using Your iPad", body: "We're all busy people; surely we don't have time to make all the presentations we need to... Well, meet the answer to your problems - this 'how to' guide tells you everything you need to know to make presentations on the go using your iPad. Never waste a train journey again!")
-presentation_c.save
+Category.new(name: 'Leadership').save
+Category.new(name: 'Technology').save
+Category.new(name: 'Education').save
+Category.new(name: 'Marketing').save
+Category.new(name: 'Design').save
+Category.new(name: 'History').save
